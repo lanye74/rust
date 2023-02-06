fd9ae9b57da7e086543c8053a9bd9c2e4ec58b53
lanye74 <layne.ogden727@gmail.com>
2023-02-06 10:46:01 -0500

    cut short

--- 04.x-league-search/src/main.rs
+++ 04.x-league-search/src/main.rs
@@ -2,5 +2,8 @@ use std::{fs, io};
 
 
 fn main() {
+	let path = "../champs.txt";
 
+	// https://doc.rust-lang.org/std/error/index.html#common-message-styles
+	let champs = fs::read_to_string(path).expect("file should've been read!");
 }
