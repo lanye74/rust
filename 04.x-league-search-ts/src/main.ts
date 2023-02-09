f6fb2a741f1dbf46cffad5dbd093036650924e22
lanye74 <layne.ogden727@gmail.com>
2023-02-09 11:18:48 -0500

    it works! but now to go to rust...

--- 04.x-league-search-ts/src/main.ts
+++ 04.x-league-search-ts/src/main.ts
@@ -8,35 +8,58 @@ const champions = championsText.split("\r\n");
 
 
 
-// i don't know what i'm doing. if i can't implement this in ts i have no hope for implementing it in rust
-function search(query: string) {
-	const queryCharacters = query.split("");
+// ignore capitalization, just for emphasis
+// goal: given query "ww", I should be able to get the result "WarWick"
+// given query "di", I should be able to get both kassaDIn and morDekaIser
+// given query "nai", I should be able to get both NAmI and Nunu And wIllump
+// given query "aa", I should be able to get both AAtrox and tAliyAh
+// given query "aaa", I should be able to get both kAtArinA and mAlzAhAr
+// given query "aka", I should be able to get both AKAli and AKshAn
+// ^ this is the annoying one. it requires me to keep track of characters found and their last position
+// wait. can't I use indexOf from a starting position...?
 
+// getting closer. but not quite there
+function search(query: string) {
+	const queryCharacters = query.trim().toLowerCase().split("");
+
+	// stored as [championIndex, index of last character matched]
 	let filtered: Map<string, number> = new Map();
 
-	for(const [searchIndex, character] of Object.entries(queryCharacters)) {
-		// if(character === "a") debugger;
 
-		for(const [index, champion] of Object.entries(champions)) {
-			const characterIndex = champion.toLowerCase().lastIndexOf(character);
+	for(const [searchIndex, character] of Object.entries(queryCharacters)) {
+		for(const [championIndex, champion] of Object.entries(champions)) {
+			let characterIndex = champion.toLowerCase().indexOf(character);
 
 			if(characterIndex === -1) {
-				filtered.delete(index);
+				filtered.delete(championIndex);
 				continue;
 			}
 
-			if(!filtered.has(index) && searchIndex === "0") {
-				filtered.set(index, characterIndex);
-			} else {
-				if(filtered.get(index)! >= characterIndex) {
-					filtered.delete(index);
-				} else {
-					filtered.set(index, characterIndex);
-				}
+			if(!filtered.has(championIndex) && searchIndex === "0") {
+				filtered.set(championIndex, characterIndex);
+				continue;
 			}
 
+			if(!filtered.has(championIndex) && searchIndex !== "0") {
+				continue;
+			}
+
+			// champion exists in filtered and contains this character
+			characterIndex = champion.toLowerCase().indexOf(character, filtered.get(championIndex)! + 1);
+			if(characterIndex === -1) {
+				// character exists in the string, but it comes before the most recent found character
+				filtered.delete(championIndex);
+				continue;
+			}
+
+			// we're good!
+			filtered.set(championIndex, characterIndex);
+
+			console.log(filtered)
 		}
 	}
+
+	// debugger
 
 	let filteredChampions = Array.from(filtered.keys());
 
