c8f2cc4555c386c2869f88c66e2675da40abd897
lanye74 <layne.ogden727@gmail.com>
2023-02-10 10:45:47 -0500

    i don't know what i'm doing! maybe i should just keep working on the book and come back to this later

--- 04.x-league-search/src/main.rs
+++ 04.x-league-search/src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs, io::{self, BufRead, BufReader}};
+use std::{fs, io::{self, BufRead, BufReader}, collections::HashMap};
 
 // the more i delve into this the more i realize that it's way out of my... league (badum-tshh)
 
@@ -26,15 +26,12 @@ fn main() {
 		.collect();
 
 
-
 	println!("Enter your query...");
 	let input = read_line();
 
 	// let input_chars: Vec<char> = input.chars().map(|char| char.to_ascii_lowercase()).collect();
-	let input_chars: Vec<char> = input.to_ascii_lowercase().chars().collect();
+	let input_chars: Vec<char> = input.trim().to_ascii_lowercase().chars().collect();
 
-	// https://doc.rust-lang.org/book/ch08-01-vectors.html
-	let mut output_vec: Vec<&str> = vec![];
 
 
 	// starting to realize this might not be that feasible until i learn how to make my own data structures
@@ -43,14 +40,61 @@ fn main() {
 	// might need to make an implementation in typescript to get a better grasp of what i should be doing
 
 
-	// for()
+
+	let mut filtered: HashMap<String, i32> = HashMap::new();
+
+	// https://stackoverflow.com/a/66289009
+	let binding = champs_vec.clone();
+	let _champions_iterable = binding.iter().enumerate();
+	let characters_iterable = input_chars.iter().enumerate();
+
+
+	for (search_index, character) in characters_iterable {
+		// because of variable moving and whatnot i have to clone this
+		let champions_iterable = _champions_iterable.clone();
+
+		for (champion_index, champion) in champions_iterable {
+			// let character_index = champion.to_ascii_lowercase().find(character);
+		}
+	}
 
 
 
+	// let champs_output = output_vec.join(", ");
 
-	let champs_output = output_vec.join(", ");
+	// let champs_output = filtered.keys()
 
-	println!("Champions matching your search: {}", champs_output);
+	// println!("Champions matching your search: {}", champs_output);
+
+	println!("{}", find_from_position("hello", 'h', 1));
+}
+
+
+
+fn find_from_position(string: &str, character: char, position: usize) -> isize {
+	// this is certainly one the functions i will write
+	let len = string.len();
+	let mut characters = string.chars();
+
+	let mut i = position;
+	let mut found = false;
+
+	while i < len {
+		// let
+
+		let char = match characters.nth(i).unwrap() {
+			Some(n) => {
+
+			},
+			None => {
+
+			}
+		}
+
+		i += 1;
+	}
+
+	return if found == true {i.try_into().unwrap()} else {-1};
 }
 
 
