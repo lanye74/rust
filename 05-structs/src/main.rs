3b3247b88f59b4b960cef17125f7694fcafd65aa
lanye74 <layne.ogden727@gmail.com>
2023-02-14 10:49:39 -0500

    learn struct tuples

--- 05-structs/src/main.rs
+++ 05-structs/src/main.rs
@@ -2,26 +2,33 @@ struct Champion {
 	name: String,
 	resource: String,
 	attack_type: String,
-	role: Vec<String>
+	roles: Vec<String>
 }
 
 
 
+// ??? println was whining at me to use it
+// apparently it allows me to use the Debug format {:?}
+#[derive(Debug)]
+struct Point(f32, f32, f32);
+
+
+
 fn main() {
-    println!("Hello, world!");
+	   println!("Hello, world!");
 
 	let kayn = Champion {
 		name: String::from("Kayn"),
 		resource: String::from("Mana"),
 		attack_type: String::from("Melee"),
-		role: vec![String::from("Jungle")]
+		roles: vec![String::from("Jungle")]
 	};
 
 	// constructing structs can use the x: x, --> x, shorthand
 
 	let diana = Champion {
 		name: String::from("Diana"),
-		role: vec![String::from("Jungle"), String::from("Mid")],
+		roles: vec![String::from("Jungle"), String::from("Mid")],
 		// "struct update syntax"
 		..kayn
 	};
@@ -31,4 +38,25 @@ fn main() {
 
 	// not valid; value was moved
 	// println!("{}", kayn.attack_type);
+
+
+	// tuple structs
+	// it's basically a way to give a tuple type a name
+	let my_point = Point(-3.0, 5.0, 2.0);
+	let my_other_point = Point(-1.0, 7.0, 6.0);
+
+
+	println!("average of points: {:?}", midpoint(&my_point, &my_other_point));
+}
+
+
+
+fn midpoint(n: &Point, m: &Point) -> Point {
+	return Point(mean(n.0, m.0), mean(n.1, m.1), mean(n.2, m.2));
+}
+
+
+
+fn mean(x: f32, y: f32) -> f32 {
+	return (x + y) / 2.0;
 }
