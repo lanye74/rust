2f02e7582cbf8f72bd0a10b4239a9bd4a4552986
lanye74 <layne.ogden727@gmail.com>
2023-02-15 10:06:37 -0500

    finish structs and methods

--- 05-structs/src/main.rs
+++ 05-structs/src/main.rs
@@ -2,7 +2,21 @@ struct Champion {
 	name: String,
 	resource: String,
 	attack_type: String,
-	roles: Vec<String>
+	roles: Vec<String>,
+	cs: i32
+}
+
+
+
+impl Champion {
+	// shorthand for self: &[mut] Self
+	// rust allows you to have properties and methods named the same thing,
+	// which is nice in cases like this were cs is both a noun and verb
+	fn cs(&mut self) {
+		self.cs += 4;
+	}
+
+	// functions implemented without a self ref are called associated functions
 }
 
 
@@ -21,12 +35,13 @@ fn main() {
 		name: String::from("Kayn"),
 		resource: String::from("Mana"),
 		attack_type: String::from("Melee"),
-		roles: vec![String::from("Jungle")]
+		roles: vec![String::from("Jungle")],
+		cs: 0
 	};
 
 	// constructing structs can use the x: x, --> x, shorthand
 
-	let diana = Champion {
+	let mut diana = Champion {
 		name: String::from("Diana"),
 		roles: vec![String::from("Jungle"), String::from("Mid")],
 		// "struct update syntax"
@@ -45,8 +60,21 @@ fn main() {
 	let my_point = Point(-3.0, 5.0, 2.0);
 	let my_other_point = Point(-1.0, 7.0, 6.0);
 
+	let mid = midpoint(&my_point, &my_other_point);
 
-	println!("average of points: {:?}", midpoint(&my_point, &my_other_point));
+	// :? = debug print
+	// :#? = debug pretty-print
+	println!("average of points: {:?}", &mid);
+
+	// see also: dbg! macro which prints more advanced info about line
+	// dbg! macro takes ownership and returns a new variable which allows you to do this
+	let my_other_other_point = Point(4.0, dbg!(mid.1 * 7.0), -5.0);
+
+
+	// methods on structs
+	diana.cs();
+
+	println!("diana cs: {}", diana.cs);
 }
 
 
