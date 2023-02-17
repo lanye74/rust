8bf22bdf7e41240593021db5d906a8da72d41f2b
lanye74 <layne.ogden727@gmail.com>
2023-02-17 10:11:12 -0500

    finish 6.3

--- 06.3-if-let/src/main.rs
+++ 06.3-if-let/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+	let my_value = Some(7);
+
+	// this is cringe and too verbose
+	match my_value {
+		Some(7) => println!("value is 7"),
+		// matches must be exhaustive thus this is necessary
+		_ => ()
+	};
+
+	// syntactic sugar for match basically
+	if let Some(7) = my_value {
+		println!("value is 7");
+	}
 }
