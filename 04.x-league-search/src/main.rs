eac6d3f0de3547a18c38e432f233694ab1285520
lanye74 <layne.ogden727@gmail.com>
2023-02-22 09:31:18 -0500

    done!

--- 04.x-league-search/src/main.rs
+++ 04.x-league-search/src/main.rs
@@ -1,5 +1,3 @@
-// #![allow(unused)]
-
 use std::{fs, io::{self, BufRead, BufReader}, collections::HashMap};
 
 // the more i delve into this the more i realize that it's way out of my... league (badum-tshh)
@@ -31,19 +29,10 @@ fn main() {
 	println!("Enter your query...");
 	let input = read_line();
 
-	let input_chars: Vec<char> = input.chars().map(|char| char.to_ascii_lowercase()).collect();
 	let input_chars: Vec<char> = input.trim().to_ascii_lowercase().chars().collect();
 
 
-
-	// starting to realize this might not be that feasible until i learn how to make my own data structures
-	// either i can turn these into tuples with (champ, index of matched character)
-	// or just... give up
-	// might need to make an implementation in typescript to get a better grasp of what i should be doing
-
-
-
-	let mut filtered: HashMap<String, i32> = HashMap::new();
+	let mut filtered: HashMap<usize, isize> = HashMap::new();
 
 	// https://stackoverflow.com/a/66289009
 	let binding = champs_vec.clone();
@@ -56,35 +45,71 @@ fn main() {
 		let champions_iterable = _champions_iterable.clone();
 
 		for (champion_index, champion) in champions_iterable {
-			let mut character_index = champion.to_ascii_lowercase().find(*character);
+			let champion_lower = champion.to_ascii_lowercase();
 
-			let mut character_index = unwrap_index(character_index);
+			// location of character in string
+			let character_index = unwrap_usize(champion_lower.find(*character));
+
+			let key_exists = filtered.contains_key(&champion_index);
+
+
+			// character is not in string. don't bother
+			if character_index == -1 {
+				filtered.remove(&champion_index);
+				continue;
+			}
+
+
+			// character is in string, but it's the first pass and the champion isn't in the hashmap. set it
+			if !key_exists && search_index == 0 {
+				filtered.insert(champion_index, character_index);
+				continue;
+			}
+
+			// the champion isn't in the hashmap and contains the target character;
+			// but it isn't the first pass, so it was removed for a reason. ignore it
+			if !key_exists && search_index != 0 {
+				continue;
+			}
+
+
+			// champion exists in the map and contains this character
+			let old_position = filtered.get(&champion_index);
+			let old_position = old_position.unwrap_or(&-1);
+
+			// find the location of the character and whether or not it came after the most recent found character
+			let character_index = find_from_position(champion_lower, *character, old_position + 1);
 
 			if character_index == -1 {
-				// filtered.remove(&champion_index);
+				// character exists in the champion name, but it comes before the most recent found character
+				filtered.remove(&champion_index);
+				continue;
 			}
+
+			// champion has the desired character present after the last matched one. we're good!
+			filtered.insert(champion_index, character_index);
 		}
 	}
 
 
+	let champs_output = filtered.keys();
+	let champs_output = champs_output.map(|champ| champs_vec[*champ].clone());
 
-	// let champs_output = output_vec.join(", ");
+	let mut champs_output = champs_output.collect::<Vec<String>>();
 
-	// let champs_output = filtered.keys()
+	champs_output.sort();
 
-	// println!("Champions matching your search: {}", champs_output);
-
-	println!("{}", find_from_position(String::from("hello"), 'l', 4));
+	println!("Matched champions include: {}", champs_output.join(", "));
 }
 
 
 
-fn find_from_position(string: String, character: char, position: usize) -> isize {
+fn find_from_position(string: String, character: char, position: isize) -> isize {
 	// this is certainly one the functions i will write
 	// update: having rewritten it, it isn't really that scary
 
 	for (index, char) in string.chars().enumerate() {
-		if index < position {
+		if index < usize::try_from(position).ok().unwrap() {
 			continue;
 		}
 
@@ -98,7 +123,7 @@ fn find_from_position(string: String, character: char, position: usize) -> isize
 
 
 
-fn unwrap_index(index: Option<usize>) -> isize {
+fn unwrap_usize(index: Option<usize>) -> isize {
 	return match index {
 		Some(n) => n as isize,
 		None => -1
