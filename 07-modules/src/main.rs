a5ea5dc5c9574bb234284f45d78753f234c956f0
lanye74 <layne.ogden727@gmail.com>
2023-02-28 10:13:48 -0500

    finish chapter 7

--- 07-modules/src/main.rs
+++ 07-modules/src/main.rs
@@ -16,13 +16,34 @@ mod world {
 
 
 
+// pub use world::hello;
+// re-export so that external files can now call lib::hello::say();
+
+
+
+mod module2 {
+	// use statements must be module level
+	use crate::world::hello as HelloModule;
+
+	pub fn speak() {
+		HelloModule::say();
+	}
+}
+
+
+
 
 fn main() {
 	hello_world::say();
 
+	// absolute path:
+	// crate::world::hello::say();
 	world::hello::say();
-
 
 	// use world::hello;
 	// hello::say();
+
+
+	module2::speak();
+
 }
