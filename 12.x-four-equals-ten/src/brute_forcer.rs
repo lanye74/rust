cacbc9f2a4db63aa7ddc9ed4a398c90cb3106414
LayneTheEpic <50891500+LayneTheEpic@users.noreply.github.com>
2023-04-24 23:23:14 -0400

    move unwrap_token

--- 12.x-four-equals-ten/src/brute_forcer.rs
+++ 12.x-four-equals-ten/src/brute_forcer.rs
@@ -4,7 +4,7 @@ use crate::evaluator::evaluator;
 
 pub fn brute_force(_input: &Vec<u8>) -> String {
 	// evaluator::evaluate("(3*5)/7+0-1*2*9/(8+4)*6".to_string());
-	dbg!(evaluator::evaluate(String::from("6*(1/9*9+2)+3/2")));
+	dbg!(evaluator::evaluate(String::from("2*3+4/0")));
 
 	return String::from("");
 }
--- 12.x-four-equals-ten/src/util.rs
+++ 12.x-four-equals-ten/src/util.rs
@@ -22,12 +22,10 @@ pub fn vecs_are_equal<T: std::cmp::PartialEq>(vec1: Vec<T>, vec2: Vec<T>) -> boo
 
 
 pub fn find_token(vec: &Vec<Token>, token: Token) -> usize {
-	// println!("    find_token: searching for {:?}", token);
 	for index in 0..(vec.len()) {
 		let element = &vec[index];
 
 		if *element == token {
-			// println!("    find_token: found {:?} at position {}", token, index);
 			return index;
 		}
 	}
@@ -38,12 +36,10 @@ pub fn find_token(vec: &Vec<Token>, token: Token) -> usize {
 
 
 pub fn find_token_in_range(vec: &Vec<Token>, token: Token, lower_bound: usize, upper_bound: usize) -> usize {
-	// println!("find_token_i_r: searching for {:?} from range {} to {}", token, lower_bound, upper_bound);
 	for index in lower_bound..=upper_bound {
 		let element = &vec[index];
 
 		if *element == token {
-			// println!("find_token_i_r: found {:?} at position {}", token, index);
 			return index;
 		}
 	}
@@ -53,22 +49,17 @@ pub fn find_token_in_range(vec: &Vec<Token>, token: Token, lower_bound: usize, u
 
 
 
+pub fn unwrap_token(number: &Token) -> f32 {
+	return match number {
+		Token::Number(value) => *value,
+		_ => panic!("value_from_token called with non-number!")
+	};
+}
+
+
+
 // fn find_token(input: &Vec<Token>, item: Token) -> usize {
 // 	return input.iter()
 // 		.position(|token| *token == item)
 // 		.unwrap_or(usize::MAX);
-// }
-
-
-
-// pub fn find_token_from_position(vec: &Vec<Token>, token: Token, position: usize) -> usize {
-// 	for index in position..(vec.len()) {
-// 		let element = &vec[index];
-
-// 		if *element == token {
-// 			return index;
-// 		}
-// 	}
-
-// 	return usize::MAX;
 // }
