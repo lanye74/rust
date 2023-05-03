b0ab5f35083ac5819a1f67fcadb2dbcfe3dd42e6
lanye74 <layne.ogden727@gmail.com>
2023-05-03 10:40:58 -0400

    simplify input digits logic using .chars()

--- 12.x-four-equals-ten/src/main.rs
+++ 12.x-four-equals-ten/src/main.rs
@@ -11,15 +11,12 @@ fn main() {
 
 	let input_digits = io_reader.read("Enter your digits, space-seperated: ");
 
-
 	let input_digits = input_digits
-		.trim() // clean up new lines
-		.split(" ") // split by character
-		.filter(|char| *char != "") // filter out empty characters because they're there
-		.map(|char| char.parse::<u8>()) // parse character into u8
-		.map(|num| num.unwrap_or(255)) // parse returns Result, unwrap. if unvalid, set to 255
-		.filter(|num| *num < 10) // clean up values marked as invalid
-		.collect::<Vec<u8>>(); // collect to vector
+		.trim()
+		.chars()
+		.map(|x| x.to_digit(10).unwrap_or(255) as u8)
+		.filter(|num| *num < 10)
+		.collect::<Vec<u8>>();
 
 
 	if input_digits.len() != 4 {
@@ -27,19 +24,11 @@ fn main() {
 	}
 
 
-	// let available_operations = io_reader.read("Enter available operations, or leave blank for all: ");
-
-	// let available_operations = available_operations
-	// 	.trim()
-	// 	.split("")
-	// 	.collect::<Vec<&str>>();
-
-	// dbg!(available_operations);
-
-
 	// let available_operations = vec![Operation::Add, Operation::Subtract, Operation::Multiply, Operation::Divide, Operation::Parentheses];
 	// let available_operations = vec![String::from("+"), String::from("-"), String::from("*"), String::from("/"), String::from("()")];
 	// let available_operations = EnabledOperations {addition: true, subtraction: true, multiplication: true, division: true, parentheses: false};
+
+	// test value for available_operations: 3 1 3 4
 	let available_operations = String::from("+-*/");
 
 
