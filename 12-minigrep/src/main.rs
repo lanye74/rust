2f634ead983d7c5e5af685bc2be2392aef912f6e
LayneTheEpic <50891500+LayneTheEpic@users.noreply.github.com>
2023-06-12 17:21:00 -0400

    12.1 & 12.2

--- 12-minigrep/src/main.rs
+++ 12-minigrep/src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, fs};
 
 
 
@@ -6,5 +6,14 @@ fn main() {
 	// the book wants me to annotate the type. i will use turbofish instead. no one can stop me.
 	let args = env::args().collect::<Vec<String>>();
 
-	dbg!(args);
+	let query = &args[1];
+	let file_path = &args[2];
+
+	println!("Searching for {} in file {}", query, file_path);
+
+
+	let file = fs::read_to_string(file_path)
+		.expect("should've been able to read file!");
+
+	println!("File contents:\n{}", file);
 }
